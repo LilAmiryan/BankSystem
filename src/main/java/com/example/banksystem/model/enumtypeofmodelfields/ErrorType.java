71ee6fa0377555d009e2644addf1108bd3bd3bb8
Vardan <shamiryanvardan@gmail.com>
2022-05-24 16:19:22 +0400

    Address class finished

---
+++ src/main/java/com/example/banksystem/model/enumtypeofmodelfields/ErrorType.java
@@ -0,0 +1,7 @@
+package com.example.banksystem.model.enumtypeofmodelfields;
+
+public enum ErrorType {
+    NOT_FOUND,
+    ALREADY_EXISTS,
+    NOT_VALID
+}
--- src/main/java/com/example/banksystem/repository/AddressRepository.java
+++ src/main/java/com/example/banksystem/repository/AddressRepository.java
@@ -18,4 +18,6 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
                         address.getBuilding(),
                         address.getHome());
     }
+
+
 }
